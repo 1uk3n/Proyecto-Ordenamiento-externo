8d834e85fe02815ab2caaf3392160138cf355689
andy <andy@192.168.1.73>
2020-10-30 19:00:54 -0600

    Se agregó creación y eliminación de carpetas. Se pulió el código de radixSort.

---
+++ Proyecto Ordenamiento Externo/src/proyecto/ordenamiento/externo/Utilidades.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.ordenamiento.externo;
+
+import java.io.File;
+
+/**
+ *
+ * @author andy
+ */
+public class Utilidades {
+       
+    static boolean borrarDirectorio(File directorio) {
+	    File[] contenido = directorio.listFiles();
+	    if (contenido != null) {
+	        for (File archivo : contenido) {
+	            borrarDirectorio(archivo);
+	        }
+	    }
+	    return directorio.delete();
+	}
+   
+}
